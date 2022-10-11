9daefcb1b0f12a57c8b7841778bac3b1c17ef13a
BrentMeeusen <brentm@live.nl>
2022-10-11 17:32:49 +0200

    Fixed minor bugs

--- OWK_sta.py
+++ OWK_sta.py
@@ -1,17 +1,22 @@
-import numpy
+import numpy as np
 import matplotlib
 import os
 
-data = np.array()
-    
+data = np.zeros(shape=(6,6,1))
+
 j = 0
 for file_name in os.listdir('data/'):
-    file = open(file_name, 'r')
-    for l, line in enumerate(file):
-        inputs = line.split(',')
-        for k in range (6):
-            data[l][k][j] = inputs[k]
-    j += 1
+	file = open('data/' + file_name, 'r')
+	for l, line in enumerate(file):
+		inputs = line.split(',')
+		for k in range (6):
+			if j == 0:
+				data[l][k][0] = inputs[k]
+			else:
+				pass
+				# np.append(data[l][k], inputs)
+				# data[l][k].append(inputs[k])
+	j += 1
 
 print(data)
-input()
+input("Press Enter to close...")
