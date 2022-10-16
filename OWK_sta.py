07a98905286f68488383a9d7e9aa7ccf6c23f6b1
Dirk Jan <dirkjansomsen@hotmail.com>
2022-10-16 19:45:13 +0200

    added data 13-15 + added averges chart

--- OWK_sta.py
+++ OWK_sta.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib
 import os
 from Bar_functions import *
+from Totals import *
 
 data_points = 0
 for path in os.listdir('data/'):
@@ -10,14 +11,14 @@ for path in os.listdir('data/'):
 data = np.zeros(shape=(6,6,data_points))
 
 j = 0
+# format: data[colour][question][data_point]
 for file_name in os.listdir('data/'):
 	file = open('data/' + file_name, 'r')
 	for l, line in enumerate(file):
 		inputs = line.split(',')
 		for k in range (6):
 			if j == 0:
-				data[l
-				][k][0] = int(inputs[k])
+				data[l][k][0] = int(inputs[k])
 			else:
 				data[l][k][j] = int(inputs[k])
 	j += 1
@@ -28,5 +29,5 @@ occurrence_bar(get_occurrence_count(data, 2), "bar_C")
 occurrence_bar(get_occurrence_count(data, 3), "bar_D")
 occurrence_bar(get_occurrence_count(data, 4), "bar_E")
 occurrence_bar(get_occurrence_count(data, 5), "bar_F")
-
-input("Press Enter to close...")
+plot_averages(data, data_points)
+#input("Press Enter to close...")
---
+++ Totals.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def get_totals(data):
+    to_return = np.zeros(shape=(6, 6))
+
+    for i in range(6):
+        for j in range(6):
+            for k in data[i][j]:
+                to_return[i][j] += k
+    return to_return
+
+
+def get_averages(data, data_points):
+    totals = get_totals(data)
+    to_return = np.zeros(shape=(6, 6))
+
+    for i in range(6):
+        for j in range(6):
+            to_return[i][j] = totals[i][j] / data_points
+    return to_return
+
+
+def plot_averages(data, data_points):
+    averages = get_averages(data, data_points)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    x = [1, 2, 3, 4, 5, 6]
+    ax.set_xticks(np.arange(6)+1,('A', 'B', 'C', 'D', 'E', 'F'))
+    y_red = averages[0]
+    y_orange = averages[1]
+    y_yellow = averages[2]
+    y_green = averages[3]
+    y_blue = averages[4]
+    y_purple = averages[5]
+
+    ax.scatter(x, y_red, c="red")
+    ax.scatter(x, y_orange, c="orange")
+    ax.scatter(x, y_yellow, c="yellow")
+    ax.scatter(x, y_green, c="green")
+    ax.scatter(x, y_blue, c="blue")
+    ax.scatter(x, y_purple, c="purple")
+
+    plt.savefig('charts/averages.png')
