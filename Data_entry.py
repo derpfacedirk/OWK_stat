81657ec77b77e0c19c6ff097e68afb98cb44123d
Dirk Jan <dirkjansomsen@hotmail.com>
2022-11-29 20:23:48 +0100

    bugfixing

--- Data_entry.py
+++ Data_entry.py
@@ -7,20 +7,18 @@ def get_data(path):
     data_points = 0
     for i in os.listdir(path):
         data_points += 1
-
     data = np.zeros(shape=(6, 6, data_points))
 
-    j = 0
+    m = 0
     # format: data[colour][question][data_point]
     for file_name in os.listdir(path):
         file = open(path + file_name, 'r')
         for j, line in enumerate(file):
             inputs = line.split(',')
             for k in range(6):
-                if j == 0:
+                if m == 0:
                     data[j][k][0] = int(inputs[k])
                 else:
-                    data[j][k][j] = int(inputs[k])
-        j += 1
-
+                    data[j][k][m] = int(inputs[k])
+        m += 1
     return data
